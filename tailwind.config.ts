a05fd86d7f82c372d0ff7f2bfa71200623833d63
Nicobonilla <n.bonillamarin@gmail.com>
2024-09-30 14:54:31 -0300

    Se agrego componente Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,21 @@ import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
 export default {
-  content: ["./app/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)"],
       },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        menuColor: '#1C1F2A',
+        menuColor2: '#303649',
+      },
+      fontSize: {
+        sm: '0.6rem'
+      }
     },
   },
   future: {
