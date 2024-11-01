f0f64d02ed52e461e08ccd7d8b440ac5371b3c30
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-11-01 19:36:49 -0300

    fix type

--- components/shared/Component/ItemBanner.tsx
+++ components/shared/Component/ItemBanner.tsx
@@ -7,21 +7,21 @@ export default function ItemBanner({ items }: { items: Item[] | null }) {
     return <p>No items available</p>;
   }
 
-  const Item: React.FC<Item> = ({ image, title, description }: Item) => {
+  const Item: React.FC<Item> = (item: Item) => {
     return (
       <div className="mb-10 flex flex-col items-center text-center md:mb-0">
         <div className="relative flex h-[200px] w-full max-w-[380px] justify-center md:h-[160px] md:max-w-[300px] lg:h-[200px] lg:max-w-[300px]">
           <div className="absolute inset-0 rounded-xl bg-gradient-to-b from-gray-200 to-transparent"></div>
           <Image
-            src={urlForImage(image).url() || '/meeting.jpg'} // Fallback image if URL generation fails
-            alt={title || 'Image'}
+            src={urlForImage(item?.image).url() || '/meeting.jpg'} // Fallback image if URL generation fails
+            alt={item?.title || 'Image'}
             fill
             className="relative z-10 object-contain p-2 md:pt-5"
           />
         </div>
-        <h3 className="h3 mt-4 md:max-w-[280px]">{title || 'Title'}</h3>
+        <h3 className="h3 mt-4 md:max-w-[280px]">{item?.title || 'Title'}</h3>
         <p className="p3 mt-2 md:max-w-[300px]">
-          {description || 'Description'}
+          {item?.description || 'Description'}
         </p>
       </div>
     );
