468b596154c9dc58030ad3e11108fd9ce9dec7e4
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-11-05 18:54:56 -0300

    fix export null

--- sanity/fetchs/bannerFetch.ts
+++ sanity/fetchs/bannerFetch.ts
@@ -1,4 +1,4 @@
-import { GetBannerDataQueryResult, GetPagesQueryResult } from '@/sanity.types';
+import { GetBannerDataQueryResult } from '@/sanity.types';
 
 // This represents a single banner object
 export type BannerData = GetBannerDataQueryResult[number];
