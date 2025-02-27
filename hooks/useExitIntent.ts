c968bc230933292045281d2a0a80c018e957ed0b
Nicolas Bonilla <n.bonillamarin@gmail.com>
2025-02-26 22:36:54 -0300

    fix useclient of hook

--- hooks/useExitIntent.ts
+++ hooks/useExitIntent.ts
@@ -1,35 +1,40 @@
-import { useEffect, useRef, useState } from "react";
+'use client';
+import { useEffect, useRef, useState } from 'react';
 
 const useExitIntent = (onExit: () => void, delay = 3) => {
   const [hasInteracted, setHasInteracted] = useState(false);
   const hasFired = useRef(false); // Para asegurarnos de que solo se ejecuta una vez
 
   useEffect(() => {
+    if (typeof window === 'undefined') return;
     const handleMouseMove = (event: MouseEvent) => {
       if (!hasInteracted || hasFired.current) return;
 
-      const isExiting = !event.relatedTarget && event.clientY <= 0 && event.movementY < -10;
+      const isExiting =
+        !event.relatedTarget && event.clientY <= 0 && event.movementY < -10;
 
       if (isExiting) {
         hasFired.current = true; // Evita disparar el evento múltiples veces
         onExit();
-        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener('mousemove', handleMouseMove);
       }
     };
 
     const handleUserInteraction = () => {
       setTimeout(() => setHasInteracted(true), delay * 1000);
-      document.removeEventListener("mousemove", handleUserInteraction); // Corrige la eliminación de eventos
+      document.removeEventListener('mousemove', handleUserInteraction); // Corrige la eliminación de eventos
     };
 
     if (window.innerWidth > 768) {
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mousemove", handleUserInteraction, { once: true });
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('mousemove', handleUserInteraction, {
+        once: true,
+      });
     }
 
     return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mousemove", handleUserInteraction);
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mousemove', handleUserInteraction);
     };
   }, [hasInteracted, onExit, delay]);
 };
