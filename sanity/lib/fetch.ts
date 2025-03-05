7ce35ab455774102a7c49037531e3ff32f5d8839
Nicolas Bonilla <n.bonillamarin@gmail.com>
2025-03-05 00:54:39 -0300

    improve sitemap

--- sanity/lib/fetch.ts
+++ sanity/lib/fetch.ts
@@ -54,7 +54,7 @@ export async function sanityFetch<const QueryString extends string>({
     perspective: 'published',
     useCdn: false,
     next: {
-      revalidate: 20,
+      revalidate: 40000,
     },
   });
 }
