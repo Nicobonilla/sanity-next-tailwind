7c5240c3cb5af1c4fab27d25cdf5ccc6f6c79e2c
Nicolas Bonilla <n.bonillamarin@gmail.com>
2025-03-10 16:45:41 -0300

    delete contacto from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -73,12 +73,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    {
-      url: `${baseUrl}/contacto`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
     ...(servicesPages || []),
     ...(postsPages || []),
     ...(unitBusinessPages || []),
