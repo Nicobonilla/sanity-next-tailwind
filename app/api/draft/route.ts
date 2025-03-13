6bb1bf21acc092ce5b5f7a12e2a0b7e683fe8bb9
Nicolas Bonilla <n.bonillamarin@gmail.com>
2025-03-13 17:04:56 -0300

    fix token undefined case

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -8,6 +8,9 @@ import { defineEnableDraftMode } from 'next-sanity/draft-mode';
 import { client } from '@/sanity/lib/client';
 import { token } from '@/sanity/lib/token';
 
-export const { GET } = defineEnableDraftMode({
+if (!token) {
+  throw new Error('Missing SANITY_API_READ_TOKEN');
+}
+export const { GET } =  defineEnableDraftMode({
   client: client.withConfig({ token }),
 });
